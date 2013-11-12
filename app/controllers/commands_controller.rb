84abc219a67cc84ba12370eb176a4e13fddf2cf0
efavre <efavre@octo.com>
2013-11-12 21:36:09 +0100

    add system command call

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -2,16 +2,21 @@ class CommandsController < ApplicationController
 	
 	def create
 		error = false
+		cmd = "echo 'error'"
 		if params.include?(:command)
 			case params[:command]
 			when "up"
 				p "GOING FORWARD"
+				cmd = "echo 'forward' >> test.txt"
 			when "down"
 				p "GOING BACKWARD"
+				cmd = "echo 'backward' >> test.txt"
 			when "left"
 				p "TURNING LEFT"
+				cmd = "echo 'left' >> test.txt"
 			when "right"
 				p "TURNING RIGHT"
+				cmd = "echo 'right' >> test.txt"
 			else
 				p "ERROR"
 				error = true
@@ -22,6 +27,7 @@ class CommandsController < ApplicationController
 		if error
 			render :json => "KO"
 		else
+			value = `#{cmd}`
 			render :json => "OK"
 		end
 	end
